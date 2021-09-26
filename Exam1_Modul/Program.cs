9438371fe91c7c04b1726b65fd81d3c83990c66d
Funkyman3438@mail.ru <Funkyman3438@mail.ru>
2021-09-26 18:56:08 +0300

    Я написал функцию для калькулятора !

--- Exam1_Modul/Program.cs
+++ Exam1_Modul/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Exam1_Modul
 {
+    class Calculator
+    {
+        public void charchters()
+        {
+            int a = 1;
+            double b;
+            int c = 3 ;
+            b = a + c;
+            Console.WriteLine("Сумма а и с равна:" + b);
+                
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator sum = new Calculator();
+            sum.charchters();
+            Console.ReadKey();
         }
     }
 }
